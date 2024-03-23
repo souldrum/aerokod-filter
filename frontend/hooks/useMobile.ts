8c7a600e890d2c04b06c9e45655f25f5fe7eb71e
souldrum <soul_20585@mail.ru>
2024-03-23 14:09:25 +0500

    add useMobile for fix conflict isMobil with SSR

--- frontend/hooks/useMobile.ts
+++ frontend/hooks/useMobile.ts
@@ -1,9 +1,20 @@
+import React from "react";
 import { UAParser } from "ua-parser-js";
 
-export const isMobile = () => {
+const isMobile = () => {
   const uaMobile = UAParser(navigator.userAgent).ua.includes("Mobile");
   const maxWidth = window.matchMedia("(max-width: 768px)").matches;
   const pointerCoarse = window.matchMedia("(pointer:coarse)").matches;
 
   return uaMobile && maxWidth && pointerCoarse;
+};
+
+export const useMobile = () => {
+  const [isMobileDevice, setIsMobileDevice] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isMobile()) setIsMobileDevice(true);
+  }, [isMobileDevice]);
+
+  return isMobileDevice;
 };
